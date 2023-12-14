737883102c504a47d291a6ff5e88500ae86256e0
NatthaphongS <sinsupharoek@gmail.com>
2023-12-14 14:14:35 +0700

    trust proxy

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const rateLimitMiddleWare = require("./middlewares/rate-limit");
-const notFoundMiddleWare = require("./middlewares/not-found");
-const errorMiddleWare = require("./middlewares/error");
-const authRoute = require("./routes/auth-route");
-const menuRoute = require("./routes/menu-route");
-const orderRoute = require("./routes/order-route");
+const rateLimitMiddleWare = require('./middlewares/rate-limit');
+const notFoundMiddleWare = require('./middlewares/not-found');
+const errorMiddleWare = require('./middlewares/error');
+const authRoute = require('./routes/auth-route');
+const menuRoute = require('./routes/menu-route');
+const orderRoute = require('./routes/order-route');
 
 const app = express();
 
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
+app.set('trust proxy');
 app.use(rateLimitMiddleWare);
 app.use(express.json());
 
-app.use("/auth", authRoute);
-app.use("/menu", menuRoute);
-app.use("/order", orderRoute);
+app.use('/auth', authRoute);
+app.use('/menu', menuRoute);
+app.use('/order', orderRoute);
 
 app.use(notFoundMiddleWare);
 app.use(errorMiddleWare);
